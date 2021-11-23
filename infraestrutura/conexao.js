ae89aba07200847a8cbddfbb95d04c3880544e73
Leandro Citro De Andrade <leandro.citro@dominos.com.br>
2021-11-23 01:53:47 -0300

    API agenda petshop

---
+++ infraestrutura/conexao.js
@@ -0,0 +1,11 @@
+const atendimentos = require("../controllers/atendimentos");
+
+const mysql = (require = "mysql");
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'admin',
+    database: 'agenda-petshop'
+});
